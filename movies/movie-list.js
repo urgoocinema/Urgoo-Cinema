5c14339450c07368dc3b60a157fb7c551eb4c210
tuvshin erdene <tuvshin.erdene25@gmail.com>
2025-04-10 01:26:49 +0800

    started doing movie html

---
+++ movies/movie-list.js
@@ -0,0 +1,25 @@
+[
+    {
+      "id": 1,
+      "title": "Inception",
+      "year": 2010,
+      "genres": ["Action", "Sci-Fi", "Thriller"],
+      "rating": "8.8",
+      "synopsis": "A thief who enters the dreams of others to steal secrets from their subconscious is given the chance to have his criminal record erased.",
+      "director": "Christopher Nolan",
+      "cast": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+      "poster": "inception.jpg"
+    },
+    {
+      "id": 2,
+      "title": "The Dark Knight",
+      "year": 2008,
+      "genres": ["Action", "Crime", "Drama"],
+      "rating": "9.0",
+      "synopsis": "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.",
+      "director": "Christopher Nolan",
+      "cast": ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+      "poster": "dark_knight.jpg"
+    }
+  ]
+  
