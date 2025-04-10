0e99868c61e0393d49a75a33345f407def7c38d8
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2025-04-10 13:09:00 +0800

    cleaned up some css(removed unnecessary styles)

--- movies/movie-list.js
+++ movies/movie-list.js
@@ -1,66 +1,67 @@
 [
-    {
-        "id": 1,
-        "title": "A Minecraft Movie",
-        "year": 2025,
-        "genres": ["Adventure", "Fantasy", "Family"],
-        "rating": "PG",
-        "synopsis": "Four misfits are transported to the blocky, creative world of Minecraft, where they must work together to find a way home.",
-        "director": "Jared Hess",
-        "cast": ["Jack Black", "Jason Momoa", "Emma Myers"],
-        "poster": "../pics/minecraft-portrait.jpg",
-        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
-    },
-    {
-        "id": 2,
-        "title": "Iron Winter",
-        "year": 2025,
-        "genres": ["Drama", "Adventure"],
-        "rating": "Not Rated",
-        "synopsis": "Set in the harsh Mongolian winter, the film follows young herders navigating survival, friendship, and identity amidst extreme conditions.",
-        "director": "Kasimir Burgess",
-        "cast": ["Local Mongolian actors"],
-        "poster": "../pics/minecraft-portrait.jpg",
-        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
-
-    },
-    {
-        "id": 3,
-        "title": "Mickey 17",
-        "year": 2025,
-        "genres": ["Sci-Fi", "Adventure", "Comedy"],
-        "rating": "Not Rated",
-        "synopsis": "Mickey Barnes, a space colonist, faces the challenges of being an 'expendable' crew member, encountering multiple clones of himself.",
-        "director": "Bong Joon-ho",
-        "cast": ["Robert Pattinson", "Mark Ruffalo", "Toni Collette"],
-        "poster": "../pics/minecraft-portrait.jpg",
-        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
-
-    },
-    {
-        "id": 4,
-        "title": "The King of Kings",
-        "year": 2025,
-        "genres": ["Drama", "History"],
-        "rating": "PG",
-        "synopsis": "A father tells his son the greatest story ever told, and what begins as a bedtime tale becomes a life-altering journey for both.",
-        "director": "David Lowery",
-        "cast": ["Timothée Chalamet", "Mahershala Ali", "Florence Pugh"],
-        "poster": "../pics/minecraft-portrait.jpg",
-        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
-
-    },
-    {
-        "id": 5,
-        "title": "Mission: Impossible - The Final Reckoning",
-        "year": 2025,
-        "genres": ["Action", "Adventure", "Thriller"],
-        "rating": "PG-13",
-        "synopsis": "Ethan Hunt faces his most perilous mission yet, as he confronts a shadowy adversary threatening global security.",
-        "director": "Christopher McQuarrie",
-        "cast": ["Tom Cruise", "Rebecca Ferguson", "Simon Pegg"],
-        "poster": "../pics/minecraft-portrait.jpg",
-        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
-
-    }
-]
+  {
+    id: 1,
+    title: "A Minecraft Movie",
+    year: 2025,
+    genres: ["Adventure", "Fantasy", "Family"],
+    rating: "PG",
+    synopsis:
+      "Four misfits are transported to the blocky, creative world of Minecraft, where they must work together to find a way home.",
+    director: "Jared Hess",
+    cast: ["Jack Black", "Jason Momoa", "Emma Myers"],
+    poster: "../pics/minecraft-portrait.jpg",
+    trailer: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`,
+  },
+  {
+    id: 2,
+    title: "Iron Winter",
+    year: 2025,
+    genres: ["Drama", "Adventure"],
+    rating: "Not Rated",
+    synopsis:
+      "Set in the harsh Mongolian winter, the film follows young herders navigating survival, friendship, and identity amidst extreme conditions.",
+    director: "Kasimir Burgess",
+    cast: ["Local Mongolian actors"],
+    poster: "../pics/minecraft-portrait.jpg",
+    trailer: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`,
+  },
+  {
+    id: 3,
+    title: "Mickey 17",
+    year: 2025,
+    genres: ["Sci-Fi", "Adventure", "Comedy"],
+    rating: "Not Rated",
+    synopsis:
+      "Mickey Barnes, a space colonist, faces the challenges of being an 'expendable' crew member, encountering multiple clones of himself.",
+    director: "Bong Joon-ho",
+    cast: ["Robert Pattinson", "Mark Ruffalo", "Toni Collette"],
+    poster: "../pics/minecraft-portrait.jpg",
+    trailer: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`,
+  },
+  {
+    id: 4,
+    title: "The King of Kings",
+    year: 2025,
+    genres: ["Drama", "History"],
+    rating: "PG",
+    synopsis:
+      "A father tells his son the greatest story ever told, and what begins as a bedtime tale becomes a life-altering journey for both.",
+    director: "David Lowery",
+    cast: ["Timothée Chalamet", "Mahershala Ali", "Florence Pugh"],
+    poster: "../pics/minecraft-portrait.jpg",
+    trailer: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`,
+  },
+  {
+    id: 5,
+    title: "Mission: Impossible - The Final Reckoning",
+    year: 2025,
+    genres: ["Action", "Adventure", "Thriller"],
+    rating: "PG-13",
+    synopsis:
+      "Ethan Hunt faces his most perilous mission yet, as he confronts a shadowy adversary threatening global security.",
+    director: "Christopher McQuarrie",
+    cast: ["Tom Cruise", "Rebecca Ferguson", "Simon Pegg"],
+    poster: "../pics/minecraft-portrait.jpg",
+    trailer: `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`,
+  },
+];
