dd101a9ba7f4010212b5d660b797c3f6c8e03f4a
tuvshin erdene <tuvshin.erdene25@gmail.com>
2025-04-10 10:06:11 +0800

    started movie pag

--- movies/movie-list.js
+++ movies/movie-list.js
@@ -8,7 +8,8 @@
         "synopsis": "Four misfits are transported to the blocky, creative world of Minecraft, where they must work together to find a way home.",
         "director": "Jared Hess",
         "cast": ["Jack Black", "Jason Momoa", "Emma Myers"],
-        "poster": "a_minecraft_movie.jpg"
+        "poster": "../pics/minecraft-portrait.jpg",
+        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
     },
     {
         "id": 2,
@@ -19,7 +20,9 @@
         "synopsis": "Set in the harsh Mongolian winter, the film follows young herders navigating survival, friendship, and identity amidst extreme conditions.",
         "director": "Kasimir Burgess",
         "cast": ["Local Mongolian actors"],
-        "poster": "iron_winter.jpg"
+        "poster": "../pics/minecraft-portrait.jpg",
+        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
+
     },
     {
         "id": 3,
@@ -30,7 +33,9 @@
         "synopsis": "Mickey Barnes, a space colonist, faces the challenges of being an 'expendable' crew member, encountering multiple clones of himself.",
         "director": "Bong Joon-ho",
         "cast": ["Robert Pattinson", "Mark Ruffalo", "Toni Collette"],
-        "poster": "mickey_17.jpg"
+        "poster": "../pics/minecraft-portrait.jpg",
+        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
+
     },
     {
         "id": 4,
@@ -41,7 +46,9 @@
         "synopsis": "A father tells his son the greatest story ever told, and what begins as a bedtime tale becomes a life-altering journey for both.",
         "director": "David Lowery",
         "cast": ["Timothée Chalamet", "Mahershala Ali", "Florence Pugh"],
-        "poster": "the_king_of_kings.jpg"
+        "poster": "../pics/minecraft-portrait.jpg",
+        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
+
     },
     {
         "id": 5,
@@ -52,6 +59,8 @@
         "synopsis": "Ethan Hunt faces his most perilous mission yet, as he confronts a shadowy adversary threatening global security.",
         "director": "Christopher McQuarrie",
         "cast": ["Tom Cruise", "Rebecca Ferguson", "Simon Pegg"],
-        "poster": "mission_impossible_final_reckoning.jpg"
+        "poster": "../pics/minecraft-portrait.jpg",
+        "trailer": `<iframe width="560" height="315" src="https://www.youtube.com/embed/8B1EtVPBSMw?si=n5rx5_91TFwoMiN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"`
+
     }
 ]
