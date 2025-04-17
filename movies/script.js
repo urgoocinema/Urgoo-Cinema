432789716892e34c240c4fe97e38f6656a7a77de
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2025-04-17 14:59:56 +0800

    implemented seat

--- movies/script.js
+++ movies/script.js
@@ -1,4 +1,11 @@
 const seatWrapper = document.querySelector('.seatWrapper');
-for(let i = 0 ; i <33 ; i++){
-    seatWrapper.innerHTML+='<div class = "seat"></div>';
+const layout = [10,12,14,16,16,20,20,20,20,20,20,20,20,18,16];
+let seatHTML = ''
+for(let i = 0 ; i <layout.length ; i++){
+    seatHTML+='<div class="row">';
+    for(let j = 0 ; j < layout[i] ; j++){
+        seatHTML += '<div class = "seat"></div>';
+    }
+    seatHTML+='</div>';
 }
+seatWrapper.innerHTML+=seatHTML;
