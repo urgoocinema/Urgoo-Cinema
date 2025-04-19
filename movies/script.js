48178bce700da79f31ff36c9df8edda59f2211e2
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2025-04-19 17:06:39 +0800

    improved seat layout

--- movies/script.js
+++ movies/script.js
@@ -1,11 +1,26 @@
 const seatWrapper = document.querySelector('.seatWrapper');
-const layout = [10,12,14,16,16,20,20,20,20,20,20,20,20,18,26];
+
+const layout = [
+    [5,5,5,5],
+    [2,2,2,2],
+    [5,5,5,5],
+    [2,2,2,2],
+    [5,5,5,5]
+];
 let seatHTML = '';
-for(let i = 0 ; i <layout.length ; i++){
-    seatHTML+='<div class="row">';
-    for(let j = 0 ; j < layout[i] ; j++){
-        seatHTML += '<div class = "seat"></div>';
+
+layout.forEach((row) =>{
+    seatHTML+='<div class = "aisle">';
+    for(let i = 0 ; i < row.length ; i++){
+        seatHTML+='<div class = "row">';
+        for(let j = 0 ; j < row[i] ; j++){
+            seatHTML += '<div class = "seat"></div>'
+        }
+        seatHTML+='</div>';
     }
     seatHTML+='</div>';
-}
+});
+
 seatWrapper.innerHTML+=seatHTML;
+
+
