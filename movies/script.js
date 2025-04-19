c17f7593136bd42706da446a28bbd7a45cbc6636
tuvshin.erdene25@gmail.com <tuvshin.erdene25@gmail.com>
2025-04-19 17:36:24 +0800

    added seat indexes

--- movies/script.js
+++ movies/script.js
@@ -1,22 +1,34 @@
-// seat layout 
-const seatWrapper = document.querySelector('.seatWrapper');
+// seat layout
+const seatWrapper = document.querySelector(".seatWrapper");
 const layout = [
-    [5,5,5,5],
-    [2,2,2,2],
-    [5,5,5,5],
-    [2,2,2,2],
-    [5,5,5,5]
+  [5, 5, 5, 5],
+  [2, 2, 2, 2],
+  [5, 5, 5, 5],
+  [2, 2, 2, 2],
+  [5, 5, 5, 5]
 ];
-let seatHTML = '';
-layout.forEach((row) =>{
-    seatHTML+='<div class = "aisle">';
-    for(let i = 0 ; i < row.length ; i++){
-        seatHTML+='<div class = "row">';
-        for(let j = 0 ; j < row[i] ; j++){
-            seatHTML += '<div class = "seat"></div>'
-        }
-        seatHTML+='</div>';
+
+let seatHTML = "";
+let max = 0;
+
+layout.forEach((row) => {
+  if (row.length > max) {
+    max = row.length;
+  }
+  seatHTML += '<div class = "aisle">';
+  for (let i = 0; i < row.length; i++) {
+    seatHTML += '<div class = "row">';
+    for (let j = 0; j < row[i]; j++) {
+      seatHTML += '<div class = "seat"></div>';
     }
-    seatHTML+='</div>';
+    seatHTML += "</div>";
+  }
+  seatHTML += "</div>";
 });
-seatWrapper.innerHTML+=seatHTML;
+let indexHTML = '<div class = "aisle">';
+for(let i = 1 ; i <= max ; i++){
+    indexHTML+=('<div class="seat index">'+i+'</div>');
+}
+
+indexHTML += '</div>';//seat indexes
+seatWrapper.innerHTML += (indexHTML+seatHTML+indexHTML);
